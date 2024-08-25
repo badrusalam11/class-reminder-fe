56be7b51ccc1cc12a2eade716dd561660a7217a6
badrusalam11 <badrusalam760@gmail.com>
2024-08-25 15:46:16 +0700

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,13 @@ BUILD_DIR="build"
 SERVER_DIR="/var/www/html/perbanas-institute"
 SERVER_USER="vpsservice"
 SERVER_IP="103.175.217.175"
+PRIVATE_VPSSERVICE_KEY_PATH="$PRIVATE_VPSSERVICE_KEY_PATH"  # Use environment variable
 
 # Print a message to indicate the start of deployment
 echo "Starting deployment..."
 
-# Use rsync to copy files to the server
-rsync -avz --delete $BUILD_DIR/ $SERVER_USER@$SERVER_IP:$SERVER_DIR
+# Use rsync to copy files to the server with SSH key
+rsync -avz -e "ssh -i $PRIVATE_VPSSERVICE_KEY_PATH" --delete $BUILD_DIR/ $SERVER_USER@$SERVER_IP:$SERVER_DIR
 
 # Print a message indicating successful deployment
 echo "Deployment completed successfully!"
