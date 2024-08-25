f4dbf8ac9c8fdfe67ec47ff68661e025bafbe567
badrusalam11 <badrusalam760@gmail.com>
2024-08-25 14:27:19 +0700

    fix warning and add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Exit the script on any command failure
+set -e
+
+# Define variables
+BUILD_DIR="build"
+SERVER_DIR="/var/www/html/perbanas-institute"
+SERVER_USER="vpsservice"
+SERVER_IP="your_server_ip"
+
+# Print a message to indicate the start of deployment
+echo "Starting deployment..."
+
+# Use rsync to copy files to the server
+rsync -avz --delete $BUILD_DIR/ $SERVER_USER@$SERVER_IP:$SERVER_DIR
+
+# Print a message indicating successful deployment
+echo "Deployment completed successfully!"
